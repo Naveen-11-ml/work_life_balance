e0506b3a231fc71539a6a6a4a4d2b83f067639f7
dev <dev@remoteui>
2024-09-26 12:22:30 +0000

    Streamlit update

--- ui/app.py
+++ ui/app.py
@@ -1,13 +1,12 @@
-# Import Library
+from work_life_balance.modeling.predict import Predict
+
 import streamlit as st
 
-st.title("Checking Docker App")
+st.title("Work Life Balance Survey")
+st.write("Please answer the following questions to help us understand your work life balance.")
 
-st.write("Does this work?")
-
-st.subheader("One way to find out!!")
-
-check_button = st.checkbox("App On")
-
-if check_button:
-    print("Ok next step now.")
+ques, info = st.columns([0.9, 0.1])
+ques.subheader("1. On a daily diet how much Fruits/Vegetable do you eat?")
+with info.popover("i"):
+    st.write("Rate in the scale of 1 (very low within entire diet) to 5 (highly intake).")
+fruits_veggies = st.slider("Fruits/Vegetable", 1, 5)
